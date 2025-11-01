c5a1a76ddfd45869c16642e0cda995569967f663
Max <max@local>
2025-11-01 13:09:01 -0700

    part 2 a

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,89 @@
+const Header = ({ name }) => <h2>{name}</h2>
+
+const Content = (props) => (
+  <div>
+    {
+      props.parts.map((part) => (
+        <Part key={part.id} part={part}/>
+      ))
+    }
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+const Total = (props) => <p><b>Total of {props.total} exercises</b></p>
+
+const Course = ({ course }) => {
+  return(
+    <>
+      <Header name={course.name} />
+      <Content parts={course.parts}/>
+      <Total total={course.parts.reduce((sum, curr) => (sum + curr.exercises), 0)}/>
+    </>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      <h1>Web development curriculum</h1>
+      {
+        courses.map((course) => (
+          <Course key={course.id} course={course} />
+        ))
+      }
+    </div>
+  )
+}
+
+export default App
